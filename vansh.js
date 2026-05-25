66658fe65b43d78abd43e2872ee03c483e84e9d9
VanshGoyal212 <vansh.25b01010052@abes.ac.in>
2026-05-25 16:41:16 +0530

    New commits

--- vansh.js
+++ vansh.js
@@ -26,3 +26,24 @@ localStorage.setItem("name", name);
     localStorage.setItem("num2",num1);
     localStorage.setItem("num1",num2);
 }
+
+
+ function Calculate() {
+
+            let num1 = Number(document.getElementById("num1").value);
+            let num2 = Number(document.getElementById("num2").value);
+
+            let nta = ((3 * num1) - (4 * num2));
+            let lucb = ((4 * num2 / 3) - num1);
+
+            let percentage = (num2 / num1) * 100;
+
+            if (percentage >= 75) {
+                document.getElementById('result').innerHTML =
+                    "You can bunk " + Math.floor(lucb) +" lectures"+" OR "+(Math.floor(lucb)/8)+" days";
+            }
+            else {
+                document.getElementById('result').innerHTML =
+                    "You have to attend " + Math.ceil(nta) +" lectures"+" OR "+(Math.ceil(nta)/8)+" days";
+            }
+        }
